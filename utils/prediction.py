0e8a43aaf8d334dca25dc9e76dbef30e33a76ea8
Heizensky <70560945+momo1703@users.noreply.github.com>
2025-03-29 14:31:13 +0700

    Update prediction.py

--- utils/prediction.py
+++ utils/prediction.py
@@ -1,1 +1,23 @@
-prediction.py
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def predict_prices(model, X, y, scaler, tail_features):
+    predictions = model.predict(X)
+    predicted_prices = scaler.inverse_transform(
+        np.hstack((predictions, tail_features[:, 1:]))
+    )[:, 0]
+
+    actual_prices = scaler.inverse_transform(
+        np.hstack((y.reshape(-1, 1), tail_features[:, 1:]))
+    )[:, 0]
+
+    return predicted_prices, actual_prices
+
+def plot_predictions(predicted, actual):
+    fig, ax = plt.subplots()
+    ax.plot(actual, label="Actual Price")
+    ax.plot(predicted, label="Predicted Price")
+    ax.set_title("Nickel Price Forecast")
+    ax.legend()
+    st.pyplot(fig)
